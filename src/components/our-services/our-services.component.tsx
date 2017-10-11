13a600950734e87be123e8d761bbdc3085be525c
Asgeir Sognefest <sogasg@gmail.com>
2017-10-11 13:02:08 +0200

    :hammer: Refactor our-services component

--- src/components/our-services/our-services.component.tsx
+++ src/components/our-services/our-services.component.tsx
@@ -1,38 +1,56 @@
 import { Grid, Icon, Typography } from "material-ui"
 import { map } from "ramda"
 import * as React from "react"
-
-import * as styles from "./our-services.module.css"
+import { StyleRulesCallback, withStyles } from "material-ui/styles"
 
 type Props = {
   services: Array<Service>
 }
 
-const renderServices = (services: Array<Service>) =>
-  map(
-    (service: Service) => (
-      <Grid item xs={12} md={3} sm={4} key={service.header + service.icon}>
-        <div className={styles.item}>
-          <Icon color="primary" style={{ fontSize: 80 }}>
-            {service.icon}
-          </Icon>
-          <Typography type="headline" gutterBottom align="center">
-            {service.header}
-          </Typography>
-          <Typography type="body1" gutterBottom align="center">
-            {service.description}
-          </Typography>
-        </div>
-      </Grid>
-    ),
-    services
-  )
-
-export const OurServices = ({ services }: Props) => (
-  <Grid container justify="center" className={styles.outer}>
-    {renderServices(services)}
+const ourServices: React.SFC<Props & { classes: StyleClassNames }> = ({
+  classes,
+  services
+}) => (
+  <Grid container justify="center" className={classes.outer}>
+    {map(
+      (service: Service) => (
+        <Grid item xs={12} md={3} sm={4} key={service.header + service.icon}>
+          <div className={classes.item}>
+            <Icon color="primary" style={{ fontSize: 80 }}>
+              {service.icon}
+            </Icon>
+            <Typography type="headline" gutterBottom align="center">
+              {service.header}
+            </Typography>
+            <Typography type="body1" gutterBottom align="center">
+              {service.description}
+            </Typography>
+          </div>
+        </Grid>
+      ),
+      services
+    )}
   </Grid>
 )
+
+type StyleClassNames = {
+  outer: string
+  item: string
+}
+
+const styles: StyleRulesCallback = theme => ({
+  outer: {
+    backgroundColor: "#FAFAFA"
+  },
+  item: {
+    textAlign: "center",
+    padding: "2em",
+    maxWidth: 250,
+    margin: "auto"
+  }
+})
+
+export const OurServices = withStyles(styles)<Props>(ourServices)
 
 type Service = {
   header: string
