eba71556478a524ec7f9c00c408ead14a64a4a96
Asgeir Sognefest <sogasg@gmail.com>
2017-10-18 11:41:17 +0200

    :hammer: Refactor

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -4,15 +4,16 @@ import { navigateTo } from "gatsby-link"
 import * as logo from "./logo-white-transparent.png"
 
 type Props = {
-  color: "white-transparent"
   className?: string
 }
 
-export const Logo: React.SFC<Props> = props => (
+export const Logo: React.SFC<Props> = ({
+  className,
+}) => (
   <img
     src={logo}
     alt="Hallingdata logo"
-    className={props.className}
+    className={className}
     style={{cursor: "pointer"}}
     onClick={() => navigateTo("/")}
   />
