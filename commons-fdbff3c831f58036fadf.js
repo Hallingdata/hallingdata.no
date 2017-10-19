af288b5273734e3c93bee9eb420397c27d13b282
Asgeir Sognefest <sogasg@gmail.com>
2017-10-20 01:31:44 +0200

    Updates

--- commons-fdbff3c831f58036fadf.js
+++ commons-fdbff3c831f58036fadf.js
@@ -90,7 +90,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/hallingdata.no/";
+/******/ 	__webpack_require__.p = "/";
 /******/
 /******/ 	// expose the chunks object
 /******/ 	__webpack_require__.s = installedChunks;
@@ -5452,8 +5452,8 @@
 	
 	var pathPrefix = "/"; /*global __PREFIX_PATHS__, __PATH_PREFIX__ */
 	
-	if (true) {
-	  pathPrefix = ("/hallingdata.no");
+	if (false) {
+	  pathPrefix = __PATH_PREFIX__;
 	}
 	
 	function normalizePath(path) {
