4e0ed9c5a3f62b816cae9e444d496c52a12253b4
Asgeir Sognefest <sogasg@gmail.com>
2017-10-22 16:30:12 +0200

    Add CMS suppoort

--- src/html.tsx
+++ src/html.tsx
@@ -70,11 +70,27 @@ module.exports = (data: any) => {
         <meta name="twitter:title" content={page.title} />
         <meta name="twitter:description" content={page.description} />
         <meta name="twitter:image" content={page.screenshot} />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
         {css}
       </head>
       <body style={{ margin: 0 }}>
         <div id="___gatsby" dangerouslySetInnerHTML={{ __html: data.body }} />
         {data.postBodyComponents}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/index.html';
+          });
+        }
+      });
+    }
+`,
+          }}
+        />
       </body>
     </html>
   )
